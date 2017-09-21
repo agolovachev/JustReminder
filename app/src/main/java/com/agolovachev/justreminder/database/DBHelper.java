d3fa69161e8c02211fcddc58a0ac6d0892cd75fd
Andrew <gooddron134@mail.ru>
2017-09-21 14:20:02 +0600

    added task removing possibility added possibility to cancel task removing via snackbar

--- app/src/main/java/com/agolovachev/justreminder/database/DBHelper.java
+++ app/src/main/java/com/agolovachev/justreminder/database/DBHelper.java
@@ -34,6 +34,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     public static final String SELECTION_STATUS = DBHelper.TASK_STATUS_COLUMN + " = ?";
+    public static final String SELECTION_TIME_STAMP = DBHelper.TASK_TIME_STAMP_COLUMN + " = ?";
 
     private DBQueryManager queryManager;
     private DBUpdateManager updateManager;
@@ -74,5 +75,9 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public DBUpdateManager update() {
         return updateManager;
+    }
+
+    public void removeTask(long timeStamp){
+        getWritableDatabase().delete(TASKS_TABLE, SELECTION_TIME_STAMP, new String[]{Long.toString(timeStamp)});
     }
 }
--- app/src/main/java/com/agolovachev/justreminder/fragments/TaskFragment.java
+++ app/src/main/java/com/agolovachev/justreminder/fragments/TaskFragment.java
@@ -1,11 +1,17 @@
 package com.agolovachev.justreminder.fragments;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.agolovachev.justreminder.MainActivity;
+import com.agolovachev.justreminder.R;
 import com.agolovachev.justreminder.adapters.TaskAdapter;
+import com.agolovachev.justreminder.model.Item;
 import com.agolovachev.justreminder.model.ModelTask;
 
 /**
@@ -36,7 +42,7 @@ public abstract class TaskFragment extends Fragment {
     public void addTask(ModelTask newTask, boolean saveToDB) {
         int position = -1;
 
-        for (int i = 0; i < adapter.getItemCount(); i ++) {
+        for (int i = 0; i < adapter.getItemCount(); i++) {
             if (adapter.getItem(i).isTask()) {
                 ModelTask task = (ModelTask) adapter.getItem(i);
                 if (newTask.getDate() < task.getDate()) {
@@ -55,6 +61,67 @@ public abstract class TaskFragment extends Fragment {
         if (saveToDB) {
             activity.dbHelper.saveTask(newTask);
         }
+    }
+
+    public void removeTaskDialog(final int location) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+
+        dialogBuilder.setMessage(R.string.dialog_removing_message);
+
+        Item item = adapter.getItem(location);
+
+        if (item.isTask()) {
+            ModelTask removingTask = (ModelTask) item;
+
+            final long timeStamp = removingTask.getTimeStamp();
+            final boolean[] isRemoved = {false};
+
+            dialogBuilder.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    adapter.removeItem(location);
+                    isRemoved[0] = true;
+
+                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.coordinator),
+                            R.string.removed, Snackbar.LENGTH_LONG);
+                    snackbar.setAction(R.string.dialog_cancel, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            addTask(activity.dbHelper.query().getTask(timeStamp), false);
+                            isRemoved[0] = false;
+                        }
+                    });
+
+                    snackbar.getView().addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+                        @Override
+                        public void onViewAttachedToWindow(View v) {
+
+                        }
+
+                        @Override
+                        public void onViewDetachedFromWindow(View v) {
+                            if (isRemoved[0]){
+                                activity.dbHelper.removeTask(timeStamp);
+                            }
+                        }
+                    });
+
+                    snackbar.show();
+
+                    dialog.dismiss();
+                }
+            });
+
+            dialogBuilder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+        }
+
+        dialogBuilder.show();
     }
 
     public abstract void addTaskFromDB();
